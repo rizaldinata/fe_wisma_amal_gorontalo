a242492630e73a208ae87790f1ddfe78592f3ef0
ALLVVNT-code <allvvnt@gmail.com>
2025-09-25 19:38:50 +0700

    style(route): organize route constants and add comments for clarity feat(basicScaffold): add basic scaffold widget file

--- lib/core/constant/route_constant.dart
+++ lib/core/constant/route_constant.dart
@@ -1,6 +1,18 @@
 class RouteConstant {
-  static const String splash = '/';
-  static const String login = '/login';
-  static const String register = '/register';
+  // Kalau path diawali / → berarti root
+  // Kalau nggak diawali / → berarti child/nested
+
+  //splash page
+  static const String splashPath = '/';
+  static const String splash = 'splash';
+
+  //auth page
+  static const String loginPath = '/login';
+  static const String login = 'login';
+
+  static const String register = 'register';
+  static const String registerPath = '/register';
+
+  //dashboard page
   static const String dashboard = '/dashboard';
 }
